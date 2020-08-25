e0a3e9c5c6a145a13dfa017ad6cb4ead32501450
fhalamos <felipealamos@gmail.com>
2020-08-25 14:20:43 -0500

    Updating restricted words

--- restricted_words.py
+++ restricted_words.py
@@ -2,21 +2,23 @@
 #Strict = variables that if are strictly equal to column name/label, there will be a match
 
 #For location matching
-locations_fuzzy = ['district', 'country', 'subcountry', 'parish', 'village', 'community', 'location', 'panchayat', 'compound', 'survey_location', 'county', 'subcounty', 'ciudad','distrito','villa','city', 'town', 'neighbourhood']
-
-locations_strict = ['vill', 'lc']
+locations_strict = ['district', 'country', 'subcountry', 'parish', 'village', 'community', 'location', 'panchayat', 'compound', 'survey_location', 'county', 'subcounty', 'ciudad','distrito','villa','city', 'town', 'neighbourhood', 'vill', 'lc', 'barangay', 'brgy']
 
 # Flagged strings from Stata script
-stata_strict = ['nam','add','dist','parish','loc','acc','plan','medic','health','insur','num','resid','home','spec','id','enum', 'info', 'data', 'comm', 'count', 'fo']
+stata_strict = ['nam','add','addr','addr1','addr2','dist','parish','loc','acc','plan','medic','health','insur','num','resid','home','spec','id','enum', 'info', 'data', 'comm', 'count', 'fo']
 
 # Flagged strings from IPA guideline document
 ipa_strict = ['gps', 'lat', 'lon', 'coord', 'house', 'social', 'census', 'fax', 'ip', 'url', 'specify']
 
-other_strict = ['rand','uid','hh', 'age', 'gps','id', 'ip','red','fono','url', 'web', 'number', 'encuestador', 'escuela', 'colegio','edad']
+other_strict = ['rand','uid','hh', 'age', 'gps','id', 'ip','red','fono','url', 'web', 'number', 'encuestador', 'escuela', 'colegio','edad', 'insurance', 'school']
 
-fuzzy = ['name', 'fname', 'lname', 'first_name', 'last_name', 'birth', 'birthday', 'bday','address', 'school','network','gender','sex','email','child','beneficiary','mother','wife','father','husband', 'insurance', 'medical', 'enumerator', 'random', 'child_age', 'latitude', 'longitude', 'coordinates', 'website', 'address', 'nickname', 'nick_name', 'firstname', 'lastname', 'sublocation', 'alternativecontact', 'division', 'resp_name', 'resp_phone', 'head_name', 'headname', 'respname', 'subvillage', 'comment', 'contact', 'phone']
+fuzzy = ['name', 'fname', 'lname', 'first_name', 'last_name', 'birth', 'birthday', 'bday','address', 'network','gender','sex','email','child','beneficiary','mother','wife','father','husband', 'medical', 'enumerator', 'random', 'child_age', 'latitude', 'longitude', 'coordinates', 'website', 'nickname', 'nick_name', 'firstname', 'lastname', 'sublocation', 'alternativecontact', 'division', 'resp_name', 'head_name', 'headname', 'respname', 'subvillage', 'comment', 'notes', 'contact']
 
-spanish_fuzzy = ['apellido', 'apellidos', 'beneficiario', 'censo',  'comentario', 'comunidad', 'contacto', 'contar', 'coordenadas', 'direccion', 'edad_nino', 'email', 'esposa', 'esposo', 'fecha_nacimiento', 'genero', 'identificador', 'identidad', 'informacion', 'latitud', 'latitude', 'locacion', 'longitud', 'madre', 'medical', 'medico', 'nino', 'nombre', 'numero', 'padre', 'pag_web', 'pais', 'parroquia', 'primer_nombre', 'random', 'salud', 'seguro', 'sexo', 'telefono', 'tlfno', 'ubicacion', 'telefono', 'telefonico', 'teléfono', 'telefónico']
+#'phone' removing phone given that we already have the phone format detector
+#telefono', 'telefonico', 'teléfono', 'telefónico'
+#'telefono', 'tlfno'
+
+spanish_fuzzy = ['apellido', 'apellidos', 'beneficiario', 'censo',  'comentario', 'comunidad', 'contacto', 'contar', 'coordenadas', 'direccion', 'edad_nino', 'email', 'esposa', 'esposo', 'fecha_nacimiento', 'genero', 'identificador', 'identidad', 'informacion', 'latitud', 'latitude', 'locacion', 'longitud', 'madre', 'medical', 'medico', 'nino', 'nombre', 'numero', 'padre', 'pag_web', 'pais', 'parroquia', 'primer_nombre', 'random', 'salud', 'seguro', 'sexo', 'ubicacion']
 
 swahili_strict = ['jina', 'simu', 'mkoa', 'wilaya', 'kata', 'kijiji', 'kitongoji', 'vitongoji', 'nyumba', 'numba', 'namba', 'tarahe ya kuzaliwa', 'umri', 'jinsi', 'jinsia']
 
@@ -31,7 +33,7 @@ def get_strict_restricted_words():
     return list(set(strict_restricted))
 
 def get_fuzzy_restricted_words():
-    fuzzy_restricted = fuzzy + spanish_fuzzy + locations_fuzzy
+    fuzzy_restricted = fuzzy + spanish_fuzzy
     return list(set(fuzzy_restricted))
 
 #Check for repeated words in lists of strict and fuzzy
