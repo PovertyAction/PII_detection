72dddf37539efb8f53c096707ed96307bcc20930
fhalamos <felipealamos@gmail.com>
2021-02-05 11:07:21 -0300

    module for generating hmac_sha1 hashing

---
+++ hash_generator.py
@@ -0,0 +1,15 @@
+import hashlib
+import hmac
+def sha1(message):
+    return hashlib.sha1(bytes(message, encoding='utf-8')).hexdigest()
+
+def hmac_sha1(secret_key, message):
+
+    h = hmac.new(bytes(secret_key, encoding='utf-8'), msg=bytes(message, encoding='utf-8'), digestmod=hashlib.sha1)
+    print(h.hexdigest())
+
+if __name__ == '__main__':
+    print(sha1(message="The Ore-Ida brand is a syllabic abbreviation of Oregon and Idaho"))
+    #Should print 156a5e19b6301e43794afc5e5aff0584e25bfbe7
+    print(hmac_sha1(secret_key = "Secret Key", message = "Message to be sent"))
+    #Should print d5052c13e868ea7c932be9279752e9e67c8195bd
